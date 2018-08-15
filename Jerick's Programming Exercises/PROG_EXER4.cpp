c611d60ba42d87439ce69fbb1f54fac4f9f658a8
Jerick Vegile M. Cataquiz <jherick.cataquiz06@gmail.com>
2018-08-15 16:50:08 -0700

    PROG_EXER4

---
+++ Jerick's Programming Exercises/PROG_EXER4.cpp
@@ -0,0 +1,18 @@
+/*
+*AUTHOR <Jerick Vegile M. Cataquiz>
+*DATE <August 04 2019 >
+*NAME <Bags of Rice>
+*/
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int pound=2205;
+	float C,B;
+	cout << " Enter the size of the bag:";
+	cin >> C;
+	
+	 B=(pound/C);
+	 
+	cout <<" Number of bag:" <<  B;
+}
