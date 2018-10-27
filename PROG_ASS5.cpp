7be7578f9f73ff924e25ca0b3fba7bbdba819de9
Jerick Vegile M. Cataquiz <jherick.cataquiz06@gmail.com>
2018-10-27 15:15:48 +0800

    PROG_ASS5
    USING FOR LOOP

---
+++ PROG_ASS5.cpp
@@ -0,0 +1,56 @@
+/*
+*AUTHOR: < JERICK VEGILE M. CATAQUIZ >
+*DATE: < OCTOBER 26, 2018 >
+*DESCRIPTION: < USING FOR LOOP >
+*/
+#include <iostream>
+#include <unistd.h>
+ using namespace std;
+ int main()
+ {
+ 	
+ 	for (int A=0;A<1;A++)
+ 	{
+ 		cout << " Wake up in the Morning. \n";
+ 		sleep(1);
+ 			for (int A=0;A<1;A++)
+ 	{
+ 		cout << " Turn off the Alarm. \n";
+ 		sleep(1);
+ 			for (int A=0;A<1;A++)
+ 	{
+ 		cout << " Open the Data to chat my Girlfriend and Greet her Good morning. \n";
+ 		sleep(1);
+ 			for (int A=0;A<1;A++)
+ 	{
+ 		cout << " Open the Facebook and Check if there's a suspension of classes. \n";
+ 		sleep(1);
+ 			for (int A=0;A<1;A++)
+ 	{
+ 		cout << " Get up and clean the bedroom and go to the restroom to wash my face. \n";
+ 		sleep(1);
+ 		
+ 			for (int A=0;A<1;A++)
+ 	{
+ 		cout << " After that i'll get a glass of water and drink it. then make a coffee. \n";
+ 		sleep(1);
+ 			for (int A=0;A<1;A++)
+ 	{
+ 		cout << " After drinking coffee, go to the bathroom and take a bath and i'll go to school. \n";
+ 		sleep(1);
+ 			for (int A=0;A<1;A++)
+ 	{
+ 		cout << " The End....\n";
+ 		sleep(1);
+ 		
+	 }
+	 }
+ 		
+	 }
+ 		
+	 }
+	 }
+	 }
+	 }
+	 }
+ }
